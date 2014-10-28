b488936affd71f4e1caf5144a0a8f296998ab3bb
Rptx <halimsrama@gmail.com>
2014-10-27 22:50:49 -0430

    Fixed indentation and deleted some empty lines.

--- evil-surround.el
+++ evil-surround.el
@@ -36,48 +36,46 @@
 (require 'evil)
 
 (defgroup surround nil
-  "surround.vim for Emacs"
-  :prefix "surround-"
-  :group 'evil)
+	  "surround.vim for Emacs"
+	  :prefix "surround-"
+	  :group 'evil)
 
 (defcustom evil-surround-pairs-alist
-  '((?\( . ("( " . " )"))
-    (?\[ . ("[ " . " ]"))
-    (?\{ . ("{ " . " }"))
-
-    (?\) . ("(" . ")"))
-    (?\] . ("[" . "]"))
-    (?\} . ("{" . "}"))
-
-    (?# . ("#{" . "}"))
-    (?b . ("(" . ")"))
-    (?B . ("{" . "}"))
-    (?> . ("<" . ">"))
-    (?t . evil-surround-read-tag)
-    (?< . evil-surround-read-tag)
-    (?f . evil-surround-function))
-  "Association list of surround items.
+	   '((?\( . ("( " . " )"))
+	     (?\[ . ("[ " . " ]"))
+	     (?\{ . ("{ " . " }"))
+	     (?\) . ("(" . ")"))
+	     (?\] . ("[" . "]"))
+	     (?\} . ("{" . "}"))
+	     (?# . ("#{" . "}"))
+	     (?b . ("(" . ")"))
+	     (?B . ("{" . "}"))
+	     (?> . ("<" . ">"))
+	     (?t . evil-surround-read-tag)
+	     (?< . evil-surround-read-tag)
+	     (?f . evil-surround-function))
+	   "Association list of surround items.
 Each item is of the form (TRIGGER . (LEFT . RIGHT)), all strings.
 Alternatively, a function can be put in place of (LEFT . RIGHT).
 This only affects inserting pairs, not deleting or changing them."
-  :group 'surround
-  :type '(repeat (cons (regexp :tag "Key")
-                       (symbol :tag "Surround pair"))))
+:group 'surround
+:type '(repeat (cons (regexp :tag "Key")
+		     (symbol :tag "Surround pair"))))
 (make-variable-buffer-local 'evil-surround-pairs-alist)
 
 (defcustom evil-surround-operator-alist
-  '((evil-change . change)
-    (evil-delete . delete))
-  "Association list of operators to their fundamental operation.
+	   '((evil-change . change)
+	     (evil-delete . delete))
+	   "Association list of operators to their fundamental operation.
 Each item is of the form (OPERATOR . OPERATION)."
-  :group 'surround
-  :type '(repeat (cons (symbol :tag "Operator")
-                       (symbol :tag "Operation"))))
+:group 'surround
+:type '(repeat (cons (symbol :tag "Operator")
+		     (symbol :tag "Operation"))))
 
 (defcustom evil-surround-keep-point nil
-  "If set to t, the point will not move when surrounding,
-changing or deleting."
-  :group 'surround)
+	   "If set to t, the point will not move when surrounding,
+	   changing or deleting."
+	   :group 'surround)
 
 (defvar evil-surround-read-tag-map
   (let ((map (copy-keymap minibuffer-local-map)))
@@ -87,214 +85,209 @@ changing or deleting."
 
 (defmacro evil-surround-save-excursion-if-keep-point (body)
   "Executes `BODY' wrapped in a `save-excursion' if `evil-surround-keep-point'
-is non-nil.  Otherwise `BODY' is executed without
-`save-excursion'."
+  is non-nil.  Otherwise `BODY' is executed without
+  `save-excursion'."
   `(if evil-surround-keep-point (save-excursion ,body) ,body))
 
 (defun evil-surround-function ()
   "Read a functionname from the minibuffer and wrap selection in function call"
   (let ((fname (read-from-minibuffer "" "" )))
     (cons (format "%s(" (or fname ""))
-          ")")))
+	  ")")))
 
 (defun evil-surround-read-tag ()
   "Read a XML tag from the minibuffer."
   (let* ((input (read-from-minibuffer "<" "" evil-surround-read-tag-map))
-         (match (string-match "\\([0-9a-z-]+\\)\\(.*?\\)[>]*$" input))
-         (tag  (match-string 1 input))
-         (rest (match-string 2 input)))
+	 (match (string-match "\\([0-9a-z-]+\\)\\(.*?\\)[>]*$" input))
+	 (tag  (match-string 1 input))
+	 (rest (match-string 2 input)))
     (cons (format "<%s%s>" (or tag "") (or rest ""))
-          (format "</%s>" (or tag "")))))
+	  (format "</%s>" (or tag "")))))
 
 (defun evil-surround-pair (char)
   "Return the evil-surround pair of char.
-This is a cons cell (LEFT . RIGHT), both strings."
+  This is a cons cell (LEFT . RIGHT), both strings."
   (let ((pair (assoc-default char evil-surround-pairs-alist)))
     (cond
-     ((functionp pair)
-      (funcall pair))
-
-     ((consp pair)
-      pair)
-
-     (t
-      (cons (format "%c" char) (format "%c" char))))))
+      ((functionp pair)
+       (funcall pair))
+      ((consp pair) pair)
+      (t
+	(cons (format "%c" char) (format "%c" char))))))
 
 (defun evil-surround-outer-overlay (char)
   "Return outer overlay for the delimited range represented by CHAR.
-This overlay includes the delimiters.
-See also `evil-surround-inner-overlay'."
+  This overlay includes the delimiters.
+  See also `evil-surround-inner-overlay'."
   (let ((outer (lookup-key evil-outer-text-objects-map (string char))))
     (when (functionp outer)
       (setq outer (funcall outer))
       (when (evil-range-p outer)
-        (evil-surround-trim-whitespace-from-range outer "[[:space:]]")
-        (setq outer (make-overlay (evil-range-beginning outer)
-                                  (evil-range-end outer)
-                                  nil nil t))))))
+	(evil-surround-trim-whitespace-from-range outer "[[:space:]]")
+	(setq outer (make-overlay (evil-range-beginning outer)
+				  (evil-range-end outer)
+				  nil nil t))))))
 
 (defun evil-surround-trim-whitespace-from-range (range &optional regexp)
   "Given an evil-range, trim whitespace around range by shrinking the range such that it neither begins nor ends with whitespace. Does not modify the buffer."
   (let ((regexp (or regexp "[ \f\t\n\r\v]")))
     (save-excursion
       (save-match-data
-        (goto-char (evil-range-beginning range))
-        (while (looking-at regexp) (forward-char))
-        (evil-set-range-beginning range (point))
-        (goto-char (evil-range-end range))
-        (while (looking-back regexp) (backward-char))
-        (evil-set-range-end range (point))))))
+	(goto-char (evil-range-beginning range))
+	(while (looking-at regexp) (forward-char))
+	(evil-set-range-beginning range (point))
+	(goto-char (evil-range-end range))
+	(while (looking-back regexp) (backward-char))
+	(evil-set-range-end range (point))))))
 
 (defun evil-surround-inner-overlay (char)
   "Return inner overlay for the delimited range represented by CHAR.
-This overlay excludes the delimiters.
-See also `evil-surround-outer-overlay'."
+  This overlay excludes the delimiters.
+  See also `evil-surround-outer-overlay'."
   (let ((inner (lookup-key evil-inner-text-objects-map (string char))))
     (when (functionp inner)
       (setq inner (funcall inner))
       (when (evil-range-p inner)
-        (when (eq (char-syntax char) ?\()
-          (evil-surround-trim-whitespace-from-range inner "[[:space:]]"))
-        (setq inner (make-overlay (evil-range-beginning inner)
-                                  (evil-range-end inner)
-                                  nil nil t))))))
+	(when (eq (char-syntax char) ?\()
+	  (evil-surround-trim-whitespace-from-range inner "[[:space:]]"))
+	(setq inner (make-overlay (evil-range-beginning inner)
+				  (evil-range-end inner)
+				  nil nil t))))))
 
 (evil-define-motion evil-surround-line (count)
-  "Move COUNT - 1 lines down but return exclusive character motion."
-  :type exclusive
-  (let ((beg (line-beginning-position)))
-    (evil-line count)
-    (end-of-line)
-    (let ((range (evil-range beg (point) 'exclusive)))
-      (evil-expand-range range)
-      range)))
+		    "Move COUNT - 1 lines down but return exclusive character motion."
+		    :type exclusive
+		    (let ((beg (line-beginning-position)))
+		      (evil-line count)
+		      (end-of-line)
+		      (let ((range (evil-range beg (point) 'exclusive)))
+			(evil-expand-range range)
+			range)))
 
 ;;;###autoload
 (defun evil-surround-delete (char &optional outer inner)
   "Delete the surrounding delimiters represented by CHAR.
-Alternatively, the text to delete can be represented with
-the overlays OUTER and INNER, where OUTER includes the delimiters
-and INNER excludes them. The intersection (i.e., difference)
-between these overlays is what is deleted."
+  Alternatively, the text to delete can be represented with
+  the overlays OUTER and INNER, where OUTER includes the delimiters
+  and INNER excludes them. The intersection (i.e., difference)
+  between these overlays is what is deleted."
   (interactive "c")
   (cond
-   ((and outer inner)
-    (delete-region (overlay-start outer) (overlay-start inner))
-    (delete-region (overlay-end inner) (overlay-end outer))
-    (goto-char (overlay-start outer)))
-   (t
-    ;; no overlays specified: create them on the basis of CHAR
-    ;; and delete after use
-    (let* ((outer (evil-surround-outer-overlay char))
-           (inner (evil-surround-inner-overlay char)))
-      (unwind-protect
-          (when (and outer inner)
-            (evil-surround-delete char outer inner))
-        (when outer (delete-overlay outer))
-        (when inner (delete-overlay inner)))))))
+    ((and outer inner)
+     (delete-region (overlay-start outer) (overlay-start inner))
+     (delete-region (overlay-end inner) (overlay-end outer))
+     (goto-char (overlay-start outer)))
+    (t
+      ;; no overlays specified: create them on the basis of CHAR
+      ;; and delete after use
+      (let* ((outer (evil-surround-outer-overlay char))
+	     (inner (evil-surround-inner-overlay char)))
+	(unwind-protect
+	  (when (and outer inner)
+	    (evil-surround-delete char outer inner))
+	  (when outer (delete-overlay outer))
+	  (when inner (delete-overlay inner)))))))
 
 ;;;###autoload
 (defun evil-surround-change (char &optional outer inner)
   "Change the surrounding delimiters represented by CHAR.
-Alternatively, the text to delete can be represented with the
-overlays OUTER and INNER, which are passed to `evil-surround-delete'."
+  Alternatively, the text to delete can be represented with the
+  overlays OUTER and INNER, which are passed to `evil-surround-delete'."
   (interactive "c")
   (cond
-   ((and outer inner)
-    (evil-surround-delete char outer inner)
-    (evil-surround-region (overlay-start outer)
-                     (overlay-end outer)
-                     nil (read-char)))
-   (t
-    (let* ((outer (evil-surround-outer-overlay char))
-           (inner (evil-surround-inner-overlay char)))
-      (unwind-protect
-          (when (and outer inner)
-            (evil-surround-change char outer inner))
-        (when outer (delete-overlay outer))
-        (when inner (delete-overlay inner)))))))
+    ((and outer inner)
+     (evil-surround-delete char outer inner)
+     (evil-surround-region (overlay-start outer)
+			   (overlay-end outer)
+			   nil (read-char)))
+    (t
+      (let* ((outer (evil-surround-outer-overlay char))
+	     (inner (evil-surround-inner-overlay char)))
+	(unwind-protect
+	  (when (and outer inner)
+	    (evil-surround-change char outer inner))
+	  (when outer (delete-overlay outer))
+	  (when inner (delete-overlay inner)))))))
 
 ;; Dispatcher function in Operator-Pending state.
 ;; "cs" calls `evil-surround-change', "ds" calls `evil-surround-delete',
 ;; and "ys" calls `evil-surround-region'.
 (evil-define-command evil-surround-edit (operation)
-  "Edit the surrounding delimiters represented by CHAR.
-If OPERATION is `change', call `evil-surround-change'.
-if OPERATION is `surround', call `evil-surround-region'.
-Otherwise call `evil-surround-delete'."
-  (interactive
-   (progn
-     ;; abort the calling operator
-     (setq evil-inhibit-operator t)
-     (list (assoc-default evil-this-operator
-                          evil-surround-operator-alist))))
-  (evil-surround-save-excursion-if-keep-point
-    (cond 
-     ((eq operation 'change)
-      (call-interactively 'evil-surround-change))
-     ((eq operation 'delete)
-      (call-interactively 'evil-surround-delete))
-     (t
-      (define-key evil-operator-shortcut-map "s" 'evil-surround-line)
-      (call-interactively 'evil-surround-region)))))
+		     "Edit the surrounding delimiters represented by CHAR.
+		     If OPERATION is `change', call `evil-surround-change'.
+		     if OPERATION is `surround', call `evil-surround-region'.
+		     Otherwise call `evil-surround-delete'."
+		     (interactive
+		       (progn
+			 ;; abort the calling operator
+			 (setq evil-inhibit-operator t)
+			 (list (assoc-default evil-this-operator
+					      evil-surround-operator-alist))))
+		     (evil-surround-save-excursion-if-keep-point
+		       (cond 
+			 ((eq operation 'change)
+			  (call-interactively 'evil-surround-change))
+			 ((eq operation 'delete)
+			  (call-interactively 'evil-surround-delete))
+			 (t
+			   (define-key evil-operator-shortcut-map "s" 'evil-surround-line)
+			   (call-interactively 'evil-surround-region)))))
 
 (evil-define-operator evil-surround-region (beg end type char &optional force-new-line)
-  "Surround BEG and END with CHAR.
+		      "Surround BEG and END with CHAR.
 
-When force-new-line is true, and region type is not line, the
-following: (vertical bars indicate region start/end points)
+		      When force-new-line is true, and region type is not line, the
+		      following: (vertical bars indicate region start/end points)
 
-   do |:thing|
+		      do |:thing|
 
-Becomes this:
+		      Becomes this:
 
-   do {
-     :thing
-   }"
+		      do {
+		      :thing
+		      }"
 
-  (interactive "<R>c")
-  (let* ((overlay (make-overlay beg end nil nil t))
-         (pair (evil-surround-pair char))
-         (open (car pair))
-         (close (cdr pair)))
-    (unwind-protect
-        (progn
-          (goto-char (overlay-start overlay))
+		      (interactive "<R>c")
+		      (let* ((overlay (make-overlay beg end nil nil t))
+			     (pair (evil-surround-pair char))
+			     (open (car pair))
+			     (close (cdr pair)))
+			(unwind-protect
+			  (progn
+			    (goto-char (overlay-start overlay))
 
-          (cond ((eq type 'line)
-                 (insert open)
-                 (indent-according-to-mode)
-                 (newline-and-indent)
-                 (goto-char (overlay-end overlay))
-                 (insert close)
-                 (indent-according-to-mode)
-                 (newline))
-
-                (force-new-line
-                 (insert open)
-                 (indent-according-to-mode)
-                 (newline-and-indent)
-                 (goto-char (overlay-end overlay))
-                 (newline-and-indent)
-                 (insert close))
-
-                (t
-                 (insert open)
-                 (goto-char (overlay-end overlay))
-                 (insert close)))
-          (goto-char (overlay-start overlay)))
-      (delete-overlay overlay))))
+			    (cond ((eq type 'line)
+				   (insert open)
+				   (indent-according-to-mode)
+				   (newline-and-indent)
+				   (goto-char (overlay-end overlay))
+				   (insert close)
+				   (indent-according-to-mode)
+				   (newline))
+				  (force-new-line
+				    (insert open)
+				    (indent-according-to-mode)
+				    (newline-and-indent)
+				    (goto-char (overlay-end overlay))
+				    (newline-and-indent)
+				    (insert close))
+				  (t
+				    (insert open)
+				    (goto-char (overlay-end overlay))
+				    (insert close)))
+			    (goto-char (overlay-start overlay)))
+			  (delete-overlay overlay))))
 
 (evil-define-operator evil-Surround-region (beg end type char)
-  "Call surround-region, toggling force-new-line"
-  (interactive "<R>c")
-  (evil-surround-region beg end type char t))
+		      "Call surround-region, toggling force-new-line"
+		      (interactive "<R>c")
+		      (evil-surround-region beg end type char t))
 
 ;;;###autoload
 (define-minor-mode evil-surround-mode
-  "Buffer-local minor mode to emulate surround.vim."
-  :keymap (make-sparse-keymap)
-  (evil-normalize-keymaps))
+		   "Buffer-local minor mode to emulate surround.vim."
+		   :keymap (make-sparse-keymap)
+		   (evil-normalize-keymaps))
 
 ;;;###autoload
 (defun turn-on-evil-surround-mode ()
@@ -308,8 +301,8 @@ Becomes this:
 
 ;;;###autoload
 (define-globalized-minor-mode global-evil-surround-mode
-  evil-surround-mode turn-on-evil-surround-mode
-  "Global minor mode to emulate surround.vim.")
+			      evil-surround-mode turn-on-evil-surround-mode
+			      "Global minor mode to emulate surround.vim.")
 
 (evil-define-key 'operator evil-surround-mode-map "s" 'evil-surround-edit)
 (evil-define-key 'visual evil-surround-mode-map "s" 'evil-surround-region)
