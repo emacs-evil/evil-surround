430c81f2a14062a1d4e2e66f317fe22591241c7a
Henrik Lissner <henrik@lissner.net>
2016-03-20 13:00:52 -0400

    Fix #71

--- evil-surround.el
+++ evil-surround.el
@@ -295,6 +295,15 @@ Becomes this:
                    (evil-surround-block beg end char))
 
                   ((eq type 'line)
+                   (setq force-new-line
+                         (or force-new-line
+                             ;; Force newline if not invoked from an operator, e.g. VS)
+                             (eq evil-this-operator 'evil-surround-region)
+                             ;; Or on multi-line yanks (but not on single-line yanks),
+                             ;; e.g. ysj)
+                             (and (eq evil-this-operator 'evil-yank)
+                                  (/= (line-number-at-pos) (line-number-at-pos (1- end))))))
+
                    (back-to-indentation)
                    (setq beg-pos (point))
                    (insert open)
