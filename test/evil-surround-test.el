6ed075041403b8abb7672d258dcb12600d20880e
Evgeni Kolev <evgenysw@gmail.com>
2017-09-10 08:05:46 +0300

    Add examples from readme as automated tests

--- test/evil-surround-test.el
+++ test/evil-surround-test.el
@@ -14,4 +14,28 @@
       ("csb'")
       "one 'two' three"
       ("ds'")
-      "one two three")))
+      "one two three"))
+  (ert-info ("examples from readme")
+    (evil-test-buffer
+      :visual-start nil
+      :visual-end nil
+      "\"Hello world!\""
+      (turn-on-evil-surround-mode)
+      ("cs\"'")
+      "'Hello world!'"
+      ("cs'<q>")
+      "<q>Hello world!</q>"
+      ("cst\"")
+      "\"Hello world!\""
+      ("ds\"")
+      "Hello world!"
+      ("ysiw]")
+      "[Hello] world!"
+      ("cs[{")
+      "{ Hello } world!"
+      ("yssb")
+      "({ Hello } world!)"
+      ("lds{ds)") ;; 'l' to move the cursor right, inside brackets
+      "Hello world!"
+      ("ysiw<em>")
+      "<em>Hello</em> world!")))
