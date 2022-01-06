a14856cd811d2ad2cab343d1a2591e68c1e7ba6d
Stefan Kangas <stefan@marxist.se>
2022-01-06 08:15:53 +0100

    Bump version to 1.0.4

--- evil-surround.el
+++ evil-surround.el
@@ -7,7 +7,7 @@
 ;;         Vegard Øye <vegard_oye at hotmail dot com>
 ;; Current Maintainer: ninrod (github.com/ninrod)
 ;; Created: July 23 2011
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; Package-Requires: ((evil "1.2.12"))
 ;; Mailing list: <implementations-list at lists.ourproject.org>
 ;;      Subscribe: http://tinyurl.com/implementations-list
